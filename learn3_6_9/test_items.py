f60dff7a2ca32f6788a1e0c9641da48a22e4ea3a
Анна Яковлева <nikvnik989@gmail.com>
2020-09-10 15:27:46 +0300

    Test for 3 module / release

--- learn3_6_9/test_items.py
+++ learn3_6_9/test_items.py
@@ -5,4 +5,4 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_guest_should_see_login_link(browser):
     browser.get(link)
     browser.implicitly_wait(10)
-    browser.find_element_by_class_name("btn.btn-lg.btn-primary.btn-add-to-basket")
+    assert browser.find_element_by_class_name("btn.btn-lg.btn-primary.btn-add-to-basket"), "Is not button"
