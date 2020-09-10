76c65598974ce8ac898b8b1d35165cb9d7300faa
Анна Яковлева <nikvnik989@gmail.com>
2020-09-10 15:34:17 +0300

    Test for 3 module / rel

--- learn3_6_9/test_items.py
+++ learn3_6_9/test_items.py
@@ -2,7 +2,7 @@
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_guest_should_see_login_link(browser):
+def test_check_language_link_and_find_button(browser):
     browser.get(link)
     browser.implicitly_wait(10)
     assert browser.find_element_by_class_name("btn.btn-lg.btn-primary.btn-add-to-basket"), "Is not button"
